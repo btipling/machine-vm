b5ad18076fcf716e4925358afdc9161bacb48c2f
Bjorn Tipling <bjorn@imgur.com>
2017-07-19 00:24:48 -0700

    and and or gates

--- src/Machine/Gates.hs
+++ src/Machine/Gates.hs
@@ -1,7 +1,15 @@
-module Machine.Gates where
+module Machine.Gates (nand, and, or) where
+
+import           Prelude hiding (and, or)
 
 nand :: Int -> Int -> Int
 nand a b | a == 0 && b == 0 = 1
          | a == 1 && b == 0 = 1
          | a == 0 && b == 1 = 1
          | a == 1 && b == 1 = 0
+
+and :: Int -> Int -> Int
+and a b = nand 1 (nand a b)
+
+or :: Int -> Int -> Int
+or a b = nand (and a (nand a b)) (and b (nand a b))
--- test/Test.hs
+++ test/Test.hs
@@ -4,14 +4,28 @@ import qualified Data.List     as List
 import qualified Machine.Gates as Gates
 import qualified Test.HUnit    as HUnit
 
+testGate :: String -> (Int -> Int -> Int) -> [(Int, Int, Int)] -> HUnit.Test
+testGate name gate values = let
+    result = List.find (\(a, b, expectedOutcome) -> (gate a b) /= expectedOutcome) values
+    in (HUnit.TestCase $ HUnit.assertEqual (name ++ " should validate") Nothing result)
+
 testGatesNand :: HUnit.Test
-testGatesNand = let
-    values = [(0, 0, 1),
-              (1, 0, 1),
-              (0, 1, 1),
-              (1, 1, 0)]
-    result = List.find (\(a, b, expectedOutcome) -> (Gates.nand a b) /= expectedOutcome) values
-    in (HUnit.TestCase $ HUnit.assertEqual "Nand should validate" Nothing result)
+testGatesNand = testGate "Nand" Gates.nand [(0, 0, 1)
+                                           ,(1, 0, 1)
+                                           ,(0, 1, 1)
+                                           ,(1, 1, 0)]
+
+testGatesAnd :: HUnit.Test
+testGatesAnd = testGate "And" Gates.and [(0, 0, 0)
+                                        ,(1, 0, 0)
+                                        ,(0, 1, 0)
+                                        ,(1, 1, 1)]
+testGatesOr :: HUnit.Test
+testGatesOr = testGate "Or" Gates.or [(0, 0, 0)
+                                     ,(1, 0, 1)
+                                     ,(0, 1, 1)
+                                     ,(1, 1, 0)]
 
 main :: IO HUnit.Counts
-main = HUnit.runTestTT $ HUnit.TestList [testGatesNand]
+main = HUnit.runTestTT $ HUnit.TestList [testGatesNand
+                                        ,testGatesAnd]
