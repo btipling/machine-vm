6ae2abdbac7186c6b89a6f50ea7006d33c185e7e
Bjorn Tipling <bjorn@imgur.com>
2017-07-19 06:30:59 -0700

    Better gates.

--- src/Machine/Chips.hs
+++ src/Machine/Chips.hs
@@ -9,13 +9,13 @@ nand a b | a == 0 && b == 0 = 1
          | a == 1 && b == 1 = 0
 
 not :: Int -> Int
-not a = nand 1 a
+not a = nand a a
 
 and :: Int -> Int -> Int
 and a b = not (nand a b)
 
 or :: Int -> Int -> Int
-or a b = and (nand (and a b) (nand a b)) (not (and (not a) (not b)))
+or a b = nand (nand a a) (nand b b)
 
 xor :: Int -> Int -> Int
-xor a b = and (nand a b) (not (and (not a) (not b)))
+xor a b = and (or a b) (not (and a b))
