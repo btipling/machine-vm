74ec0d69831f7de56e29f43c4c6a6d00c3676eab
Bjorn Tipling <bjorn@imgur.com>
2017-08-08 11:23:12 -0700

    More comments explaining what's going on.

--- src/Machine/Memory.hs
+++ src/Machine/Memory.hs
@@ -4,9 +4,14 @@ module Machine.Memory (dff
 import qualified Control.Monad.State as State
 import qualified Machine.Gates       as Gates
 
+-- a digital flip flop stores an input and outputs that input on the next clock cycle
+-- out = input (t - 1) where t is the clock cycle count
 dff :: Bool -> State.State Bool Bool
 dff n = State.state (\s -> (s, n))
 
+-- a bit register uses the dff to either store a new value or store previos value
+-- it always outputs what what stored in it previously
+-- if load out = input (t - 1) else if not load out = out (t - 1)
 bitRegister :: Bool -> Bool -> State.State Bool Bool
 bitRegister input load = do
     currentState <- State.get
